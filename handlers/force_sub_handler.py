34ead6d5737be3d02eaa752ddfc656480e18e178
PRASAD MADHURANGA <102537177+prasad-kmd@users.noreply.github.com>
2022-08-19 13:51:42 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -32,7 +32,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/JoinOT).",
+                text="We are really sorry, You are Banned to use this Bot. Try Contact our [Support Group](https://t.me/+kM0sllYx2LgzYTg1).",
                 disable_web_page_preview=True
             )
             return 400
@@ -61,7 +61,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/JoinOT).",
+            text="Something went Wrong. Contact Our [Support Group](https://t.me/+kM0sllYx2LgzYTg1).",
             disable_web_page_preview=True
         )
         return 200
